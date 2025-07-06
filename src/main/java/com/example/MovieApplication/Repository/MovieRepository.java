a1aeb33f664a05b15d532cd166fd462332f09406
risara-jo <risharajl11234@gmail.com>
2025-07-07 02:11:31 +0530

    Movie part done!!

--- src/main/java/com/example/MovieApplication/Repository/MovieRepository.java
+++ src/main/java/com/example/MovieApplication/Repository/MovieRepository.java
@@ -3,5 +3,12 @@ package com.example.MovieApplication.Repository;
 import com.example.MovieApplication.Entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository <Movie, Long> {
+
+    Optional<List<Movie>> findByGenre(String genre);
+    Optional<List<Movie>> findByLanguage(String language);
+    Optional<Movie> findByName(String title);
 }
--- src/main/java/com/example/MovieApplication/Service/MovieService.java
+++ src/main/java/com/example/MovieApplication/Service/MovieService.java
@@ -1,7 +1,82 @@
 package com.example.MovieApplication.Service;
 
+import com.example.MovieApplication.DTO.MovieDTO;
+import com.example.MovieApplication.Entity.Movie;
+import com.example.MovieApplication.Repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public Movie addMovie(MovieDTO movieDTO){
+
+        Movie movie = new Movie();
+        movie.setName(movieDTO.getName());
+        movie.setDescription(movieDTO.getDescription());
+        movie.setGenre(movieDTO.getGenre());
+        movie.setReleaseDate(movieDTO.getReleaseDate());
+        movie.setDuration(movieDTO.getDuration());
+        movie.setLanguage(movieDTO.getLanguage());
+
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public List<Movie> getMoviesByGenre(String genre){
+        Optional<List<Movie>> listOfMovieBox = movieRepository.findByGenre(genre);
+
+        if(listOfMovieBox.isPresent()){
+            return listOfMovieBox.get();
+        }
+        else throw new RuntimeException("No movies found for genre "+genre);
+    }
+
+    public List<Movie> getMoviesByLanguage(String language){
+        Optional<List<Movie>> listOfMovieBox = movieRepository.findByLanguage(language);
+
+        if(listOfMovieBox.isPresent()){
+            return listOfMovieBox.get();
+        }
+        else throw new RuntimeException("No movies found of language "+ language);
+    }
+
+    public Movie getMovieByTitle(String title){
+        Optional<Movie> movieBox = movieRepository.findByName(title);
+
+        if(movieBox.isPresent()){
+            return movieBox.get();
+        }
+        else throw new RuntimeException("No movies found by title " + title);
+    }
+
+    public Movie updateMovie(Long id, MovieDTO movieDTO){
+
+        Movie movie = movieRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No movie found for the id "+id));
+        movie.setName(movieDTO.getName());
+        movie.setDescription(movieDTO.getDescription());
+        movie.setGenre(movieDTO.getGenre());
+        movie.setReleaseDate(movieDTO.getReleaseDate());
+        movie.setDuration(movieDTO.getDuration());
+        movie.setLanguage(movieDTO.getLanguage());
+
+        return movieRepository.save(movie);
+    }
+
+    public void deleteMovie(Long id){
+        movieRepository.deleteById(id);
+    }
+
+
 }
