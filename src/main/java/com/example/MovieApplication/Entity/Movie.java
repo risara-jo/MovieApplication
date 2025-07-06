30db793f09c6056a728552fdaf284e8611bb52b0
risara-jo <risharajl11234@gmail.com>
2025-07-06 15:10:14 +0530

    test commit 2

--- src/main/java/com/example/MovieApplication/Entity/Movie.java
+++ src/main/java/com/example/MovieApplication/Entity/Movie.java
@@ -1,14 +1,17 @@
 package com.example.MovieApplication.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
@@ -16,4 +19,7 @@ public class Movie {
     private Integer duration;
     private LocalDate releaseDate;
     private String language;
+
+    @OneToMany(mappedBy = "movie", fetch = FetchType.LAZY)
+    private List<Show> show;
 }
