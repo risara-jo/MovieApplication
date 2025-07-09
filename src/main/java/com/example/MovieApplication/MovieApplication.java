02649204017b8803ff2cdd0277e36b4d6a1cc6a0
risara-jo <risharajl11234@gmail.com>
2025-07-09 16:04:49 +0530

    add println in main. for deployment error fixing

--- src/main/java/com/example/MovieApplication/MovieApplication.java
+++ src/main/java/com/example/MovieApplication/MovieApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" Starting MovieApplication...");
 		SpringApplication.run(MovieApplication.class, args);
 	}
 
